9907fc897e165ff61571ed2df557bfa5bbe8c322
dev-kt07 <krishnatonk939@gmail.com>
2025-05-29 18:26:55 +0530

    newly modules

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   subnet_id          = "subnet-075bb40922d89bb13"
   security_group_ids = ["sg-0e584f891639bbf98"]
   tags = {
-    Name = "MyEC2Instance"
+    Name = "rds-mysql"
   }
 }
 
@@ -34,18 +34,11 @@ module "vpc" {
 }
 
 module "db_subnet_group" {
-  source      = "./modules/db_subnet_group"
+  source = "./modules/db_subnet_group"  # or a registry source
   name        = "my-db-subnet-group"
   description = "Subnet group for RDS"
-
-  subnet_ids = [
-     "subnet-0a8fc91b7a9043e29", "subnet-040b9382a8253e6d0"] # Replace with another private subnet in a different AZ
-  
-
+  subnet_ids  = ["subnet-0a8fc91b7a9043e29", "subnet-040b9382a8253e6d0"]
   tags = {
-     Name        = "my-db-subnet-group"
-     Environment = "production"
-     Team        = "devops"
-     Project     = "rds-mysql"
+    Environment = "prod"
   }
 }
