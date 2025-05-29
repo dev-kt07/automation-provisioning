b9db300dec5af72b40cef62b3efb36cba96c59a0
dev-kt07 <krishnatonk939@gmail.com>
2025-05-29 16:13:30 +0530

    updated vpc

--- main.tf
+++ main.tf
@@ -25,3 +25,10 @@ module "s3_bucket" {
     Project     = "s3-module-demo"
   }
 }
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr           = "10.1.0.0/16"
+  public_subnets     = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets    = ["10.1.101.0/24", "10.1.102.0/24"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+}
